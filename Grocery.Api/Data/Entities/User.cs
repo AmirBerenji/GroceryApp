bc1adb347f6fc4aba3e7b3eb443722cefb3157ad
Amir Berenji <a.berenji@live.com>
2024-03-25 14:14:54 +0400

    Create Project Api and Shared

---
+++ Grocery.Api/Data/Entities/User.cs
@@ -0,0 +1,55 @@
+﻿namespace Grocery.Api.Data.Entities
+{
+    public class User
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public int RoleId { get; set; }
+
+        public string  Password { get; set; }
+
+        public virtual Role Role { get; set; }
+    }
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int ParentId { get; set; }
+        public string? Credit { get; set; }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category{ get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public decimal TotalAmount { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Placed;
+        public Color Color => _orderStatuscolorsMap[Status];
+        private static readonly IReadOnlyDictionary<OrderStatus, Color> _orderStatuscolorsMap =
+            new Dictionary<OrderStatus, Color>
+            {
+                [OrderStatus.Placed] = Colors.Yellow,
+                [OrderStatus.Confirmed] = Colors.Blue,
+                [OrderStatus.Deliverd] = Colors.Green,
+                [OrderStatus.Cancelled] = Colors.Red,
+            };
+    }
+}
---
+++ Grocery.Shared/Enumerations/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Grocery.Shared.Enumerations
+{
+    public enum OrderStatus
+    {
+        Placed = 0,
+        Confirmed = 1,
+        Deliverd = 2,
+        Cancelled = 3
+
+    }
+
+}
